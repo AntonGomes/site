dbc35aac576f3c4e7c0a8839669b7f550d7e19a6
AntonGomes <aomlgomes@gmail.com>
2022-06-13 03:08:39 +0100

    added default layout to index

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,18 @@
 import styles from '../styles/pages.module.css'
 import Link from 'next/link'
 import Draggable from 'react-draggable';
+import Default from "../components/default.jsx"
 
 export default function Home() {
         
   return (
+      <Default>
       <div className={styles.landing}>
       <Link href = "./bordle">bordle</Link>
       <Link href = "./draw">guest book</Link>
       <Link href = "./about">about</Link>
       <Link href = "./posts">*posts in progress*</Link>
       </div>
+      </Default>
   )
 }
