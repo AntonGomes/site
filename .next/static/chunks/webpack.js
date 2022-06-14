ab89499f88cddf52dc082193a04e652885ab288a
AntonGomes <aomlgomes@gmail.com>
2022-06-14 16:05:41 +0100

    more lighthouse stuff booooo

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "1f05c7f80ba02f4a"; }
+/******/ 		__webpack_require__.h = function() { return "92fc8d71d0ab42ee"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
