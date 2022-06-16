d368e5cf8ddf357930852bcdc9ece665ea887f8a
AntonGomes <aomlgomes@gmail.com>
2022-06-16 12:30:32 +0100

    guestbook canvas width fix

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "61c2b65012e0f802"; }
+/******/ 		__webpack_require__.h = function() { return "64184efbdb7f47f5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
